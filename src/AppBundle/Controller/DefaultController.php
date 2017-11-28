a5bd0afe11f597b72968c3a869be94263db22f7a
= <pintos.ilya@ya.ru>
2017-11-28 19:49:12 +0300

    installing fosuserbundle, adding user control

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,17 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request): Response
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
